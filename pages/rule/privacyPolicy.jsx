6f4443885872c35b26597032c018bddef45f9906
Ervand <vip.ervand.88@gmail.com>
2024-12-01 18:41:31 +0400

    create new three rule page and components and media

--- pages/rule/privacyPolicy.jsx
+++ pages/rule/privacyPolicy.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import App from "../../components/Layouts/app";
-import AgreementIndex from "../../components/rule/agreementIndex";
 import PolicyIndex from "../../components/rule/policyIndex";
 
 const PrivacyPolicy = () => {
